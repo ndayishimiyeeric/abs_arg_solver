b89aadb049b598f9b1578717c723d4b0830a5fa7
Nderic Odalton <ndayishimiyeeric86@gmail.com>
2022-12-31 05:35:38 +0100

    Add acceptability and complete extensions

--- program.py
+++ program.py
@@ -59,3 +59,59 @@ def is_self_defending(af, subset):
             if not any(attacker in subset for attacker in af[arg]):
                 return False
     return True
+
+
+def get_arg_attackers(af, arg):
+    return set(attacker for attacker in af if arg in af[attacker])
+
+
+def is_arg_attacked(af, arg):
+    return any(arg in af[attacker] for attacker in af)
+
+
+def get_arg_attacked_by_subset(af, subset):
+    return [arg for arg in af if is_arg_attacked(af, arg) and arg not in subset]
+
+
+def is_arg_acceptable(af, arg, subset):
+    attackers = get_arg_attackers(af, arg)
+    subsetAtt = get_arg_attacked_by_subset(af, subset)
+    if attackers:
+        return all(attacker in subsetAtt and any(att in subset for att in get_arg_attackers(af, attacker)) for attacker in attackers)
+
+
+def check_args_in_relation(af, args):
+    # get all attack relations
+    attacks = []
+    for arg in af:
+        for attacked in af[arg]:
+            attacks.append((arg, attacked))
+    return all(i[0] in args or i[1] in args for i in attacks)
+
+
+if args.problem == "SE-CO":
+    complete_extensions = []
+    subset = []
+    generate_subsets(af, subset)
+
+    if check_args_in_relation(af, af.keys()):
+        if len(admissibles) > 0:
+            for adm in admissibles:
+                trueAdm = set()
+                if len(adm) > 0:
+                    for i in af.keys():
+                        if is_arg_acceptable(af, i, adm):
+                            trueAdm.add(i)
+
+                    if trueAdm == set(adm):
+                        adm.sort()
+                        if adm not in complete_extensions:
+                            complete_extensions.append(adm)
+                else:
+                    complete_extensions.append(adm)
+
+    if complete_extensions:
+        for extension in complete_extensions:
+            print(extension)
+    else:
+        print("None.")
